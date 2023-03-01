ce3767e6fd50b38ccbb6d0f20519c1c23a8980eb
peihanWang <nocab7714@gmail.com>
2023-03-02 00:03:22 +0800

    建置 deploy.sh 與 設定 vite.config.js:base

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# 发生错误时终止
+set -e
+
+# 构建
+npm run build
+
+# 进入构建文件夹
+cd dist
+
+# 放置 .nojekyll 以绕过 Jekyll 的处理。
+echo > .nojekyll
+
+# 如果你要部署到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+git push -f https://github.com/Nocab7714/week6-pehianWang.git main:gh-pages
+
+cd -
